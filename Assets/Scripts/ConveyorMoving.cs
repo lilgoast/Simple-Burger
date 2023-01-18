ed57fd31d6c302481649be59e7632b7b4123728b
lilgoast <105431041+lilgoast@users.noreply.github.com>
2023-01-18 08:18:55 +0200

    added conveyor

---
+++ Assets/Scripts/ConveyorMoving.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ConveyorMoving : MonoBehaviour
+{
+    [SerializeField] float speed = 10f;
+    Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 currentPos = rigidBody.position;
+        rigidBody.position += speed * Time.fixedDeltaTime * Vector3.right;
+        rigidBody.MovePosition(currentPos);
+    }
+}
