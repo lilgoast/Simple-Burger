7a83b250be2e2d03d2edc726f7b9c099ff4cded8
lilgoast <105431041+lilgoast@users.noreply.github.com>
2023-01-20 15:03:16 +0200

    Update BuildBurger.cs
    added random chalange

--- Assets/Scripts/BuildBurger.cs
+++ Assets/Scripts/BuildBurger.cs
@@ -1,4 +1,5 @@
 using Mono.Cecil.Cil;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildBurger : MonoBehaviour
@@ -8,11 +9,13 @@ public class BuildBurger : MonoBehaviour
     private static int ingredientLayer;
     private Transform parent;
     private GameObject nextIngredient;
+    private List<int> ingredientsList = new();
+    private int tempIngredientNum = 0;
+    private int amountOfIngredients;
 
     private void Awake()
     {
-        ingredientLayer = 0;
-        parent = gameObject.transform;
+        Init();
         GetNextIngredient();
     }
 
@@ -29,12 +32,37 @@ public class BuildBurger : MonoBehaviour
         }
     }
 
+    private void Init()
+    {
+        ingredientLayer = 0;
+        parent = gameObject.transform;
+        amountOfIngredients = Random.Range(5, 20);
+
+        for (int i = 0; i < amountOfIngredients; i++)
+        {
+            if (i == 0)
+                ingredientsList.Add(0);
+            else if(i == amountOfIngredients - 1)
+                ingredientsList.Add(0);
+            else
+                ingredientsList.Add(Random.Range(0, transparentIngredients.transform.childCount - 1));
+        }
+    }
+
     private void GetNextIngredient()
     {
-        nextIngredient = Instantiate(transparentIngredients.transform.GetChild(Random.Range(0, transparentIngredients.transform.childCount)), parent, false).gameObject;
-        nextIngredient.transform.localPosition = new Vector3(-0.065f + (ingredientLayer * 0.004f), 0.03f + (ingredientLayer * 0.006f), 0.09f + (ingredientLayer * 0.009f));
-        nextIngredient.transform.localRotation = Quaternion.Euler(0f, -65f, 110f);
-        nextIngredient.transform.localScale = Vector3.Scale(nextIngredient.transform.localScale, new Vector3(.07f, .07f, .07f));
+        if(tempIngredientNum < amountOfIngredients)
+        {
+            nextIngredient = Instantiate(transparentIngredients.transform.GetChild(ingredientsList[tempIngredientNum++]), parent, false).gameObject;
+            nextIngredient.transform.localPosition = new Vector3(-0.065f + (ingredientLayer * 0.004f), 0.03f + (ingredientLayer * 0.006f), 0.09f + (ingredientLayer * 0.009f));
+            nextIngredient.transform.localRotation = Quaternion.Euler(0f, -65f, 110f);
+            nextIngredient.transform.localScale = Vector3.Scale(nextIngredient.transform.localScale, new Vector3(.07f, .07f, .07f));
+        }
+        else
+        {
+            Debug.Log("You Win!");
+        }
+        
     }
 
     private void PlaceIngredient(Collider other)
